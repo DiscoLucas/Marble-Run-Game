2fc55b27247c9e3ecd56f50ef92fee7730541dce
DiscoLucas <47451042+DiscoLucas@users.noreply.github.com>
2022-05-27 11:35:33 +0200

    Finished CSV assignment

--- Assets/Scripts/CSVWriter.cs
+++ Assets/Scripts/CSVWriter.cs
@@ -6,28 +6,16 @@ using UnityEngine.UI;
 using System;
 using UnityEngine.InputSystem;
 
-// I AM THE CLINT EASTWOOD IF WRITING SPAGHETTI CODE!
 
 public class CSVWriter : MonoBehaviour
 {
     public string filename = "";
-    public Button startButton;
     
-    [Tooltip("Amount of time that data will be recorded for, in seconds")]
-    public int collectionTime = 5;
-    [Tooltip("Amount of time between each sample recorded, in miliseconds")]
-    public int collectionInterval = 20;
-
     [System.Serializable]
     public class Data
     {
         public string name;
-        //public int xAxis;
-        //public int yAxis;
-        //public int zAxis;
-        //xAxis = -Input.gyro.rotationRateUnbiased.x;
     }
-    [System.Serializable]
     public class DataList
     {
         public Data[] data;
@@ -38,69 +26,31 @@ public class CSVWriter : MonoBehaviour
     void Start()
     {
         filename = Application.dataPath + "/savedData.csv";
-        Button btn = startButton.GetComponent<Button>();
-        btn.onClick.AddListener(WriteCSV);
-
-        if (SystemInfo.supportsGyroscope)
-        {
-            Input.gyro.enabled = true;
-        }
-        //InputSystem.EnableDevice(UnityEngine.InputSystem.Gyroscope.current);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        var xAxis = -Input.gyro.rotationRateUnbiased.x;
-        var yAxis = -Input.gyro.rotationRateUnbiased.y;
-        var zAxis = -Input.gyro.rotationRateUnbiased.z;
-    }
-    /*
-    public void RecordGyroData()
-    {
-        for (int i = 0; i < data.Count; i++)
-        {
 
-        }
-    }
-    */
+
     /// <summary>
     /// Method for writing CSV file.
     /// Starts by checking if the list is less than 0, if it is, it adds a header.
     /// Afterwards, the local variable startTime is declared.
     /// Following, is a while loop that checks if
     /// </summary>
-    public void WriteCSV()
+    public void WriteCSV(List<string> input)
     {
-        //var xAxis = -Input.gyro.rotationRateUnbiased.x;
-        //var yAxis = -Input.gyro.rotationRateUnbiased.y;
-        //var zAxis = -Input.gyro.rotationRateUnbiased.z;
 
-        if (myDataList.data.Length > 0)
+
+        if (input.Count > 0)
         {
             TextWriter tw = new StreamWriter(filename, false);
-            tw.WriteLine("name, xAxiz, yAxis, zAxis");
-            tw.Close();
-
-
-            var startTime = DateTime.UtcNow;
-            while(DateTime.UtcNow - startTime < TimeSpan.FromSeconds(collectionTime))
-            {
-            tw = new StreamWriter(filename, true);
-                for (int i = 0; i < myDataList.data.Length; i++)
-                {
-                    tw.WriteLine(myDataList.data[i].name + "," +
-                       //myDataList.data[i].xAxis + "," +
-                       //myDataList.data[i].yAxis + "," +
-                       //myDataList.data[i].zAxis);
-                       -Input.gyro.rotationRateUnbiased.x + "," +
-                       -Input.gyro.rotationRateUnbiased.y + "," +
-                       -Input.gyro.rotationRateUnbiased.z);
-                }
-                tw.Close();
-                System.Threading.Thread.Sleep(collectionInterval);
-            }
+            tw.WriteLine("xAxiz, yAxis, zAxis");
             
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                tw.WriteLine(input[i]);
+            }
+            tw.Close();
         }
     }
     protected void OnGUI()
@@ -109,7 +59,7 @@ public class CSVWriter : MonoBehaviour
 
         GUILayout.Label("Orientation: " + Screen.orientation);
         GUILayout.Label("input.gyro.attitude: " + Input.gyro.attitude);
-        GUILayout.Label("iphone width/font: " + Screen.width + " : " + GUI.skin.label.fontSize);
+        GUILayout.Label("phone width/font: " + Screen.width + " : " + GUI.skin.label.fontSize);
     }
  
 
--- Assets/Scripts/DataInput.cs
+++ Assets/Scripts/DataInput.cs
@@ -4,20 +4,65 @@ using UnityEngine;
 
 public class DataInput : MonoBehaviour
 {
-    
+    [Header("List for data input")]
+    public List<string> dataInput = new List<string>();
 
-    // Start is called before the first frame update
+    [Header("Data collection setup")]
+    public float collectionTime = 5f;
+    public bool isCollecting;
+    public float startCollectionTime;
+
+    public CSVWriter CSVWriter;
+
+    /// <summary>
+    /// Checks if the system has a gyroscope and enables it if it does.
+    /// Also sets startCollectionTime value to collectionTime, for timer later.
+    /// </summary>
     void Start()
     {
         if (SystemInfo.supportsGyroscope)
         {
             Input.gyro.enabled = true;
+            startCollectionTime = collectionTime;
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (isCollecting == true)
+        {
+            if (collectionTime > 0)
+            {
+                collectionTime = collectionTime - Time.fixedDeltaTime;
+                DataCollection();
+            }
+            else
+            {
+                collectionTime = startCollectionTime;
+                isCollecting = false;
+                CSVWriter.WriteCSV(dataInput);
+                dataInput.Clear();
+            }
+        }
+        //Debug.Log(Input.gyro.rotationRateUnbiased);
+    }
+
+    /// <summary>
+    /// Converts gyro input to a string, for easier CSV writing.
+    /// </summary>
+    public void DataCollection()
+    {
+        string x = Input.gyro.rotationRateUnbiased.x.ToString();
+        string y = Input.gyro.rotationRateUnbiased.y.ToString();
+        string z = Input.gyro.rotationRateUnbiased.z.ToString();
+        string output = x + "," + y + "," + z;
+
+        dataInput.Add(output);
+    }
+
+    public void StartCollecting()
+    {
+        isCollecting = !isCollecting;
+        Debug.Log("isCollecting" + isCollecting);
     }
 }
